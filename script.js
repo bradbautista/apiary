e3e15e993a54bc44d4a0b040c8a268c01419d916
Brad Bautista <bradbautista@gmail.com>
2019-10-28 22:32:38 -0400

    fixed bug affecting column display in chrome, coded ingestion

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-
-
+// Enable menu behavior
 function showMenu() {
     $('form').on('click', 'button', function() {
         event.preventDefault();
@@ -12,10 +11,59 @@ function showMenu() {
         // Flip & unfilp the arrow
         $(this).children(':nth-child(2)').toggleClass('menu-icon_inactive');
         $(this).children(':nth-child(2)').toggleClass('menu-icon_active');
+        console.log(responseJson);
         
     });
 };
 
+// Retrieve the data so we can manipulate it
+function getData() {
+
+    const endpoint = 'https://api.publicapis.org/entries'
+  
+    fetch(endpoint)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function displayResults(responseJson) {
+    // if there are previous results or errors, remove them
+    console.log(responseJson);
+    $('#js-error-message').empty();
+  
+    // iterate over the response
+    // for (let i = 0; i < responseJson.data.length ; i++){
+  
+    //     if (responseJson.data[i].images.length === 0) {
+    //     $('#results-list').append(
+    //       `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].name}</a></h3>
+    //       <p class="address">${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode} ${responseJson.data[i].addresses[0].postalCode}</p>
+    //       <p>${responseJson.data[i].description}</p>
+    //       </li>
+    //       <hr>`);
+    //     } else {
+    //     $('#results-list').append(
+    //       `<li><h3><a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].name}</a></h3>
+    //       <p class="address">${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode} ${responseJson.data[i].addresses[0].postalCode}</p>
+    //       <p>${responseJson.data[i].description}</p>
+    //       <img src="${responseJson.data[i].images[0].url}">
+    //       </li>
+    //       <hr>`);
+    //     };
+  
+
+    // };
+};
+
+// $(getData);
 $(showMenu);
 
 // FOR LATER
