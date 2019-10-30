10ae63b1f06c285d4d83cae739be8b2011c69ae8
Brad Bautista <bradbautista@gmail.com>
2019-10-30 11:21:47 -0400

    category sort implemented

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ function showMenu() {
         // Flip & unfilp the arrow
         $(this).children(':nth-child(2)').toggleClass('menu-icon_inactive');
         $(this).children(':nth-child(2)').toggleClass('menu-icon_active');
-        console.log(responseJson);
         
     });
 };
 
-// Retrieve the data so we can manipulate it
+// Retrieve and return the data, then pass it to the 
+// displayResults function so we can manipulate it
 function getData() {
 
     const endpoint = 'https://api.publicapis.org/entries'
@@ -34,10 +34,17 @@ function getData() {
     });
 }
 
-function displayResults(responseJson) {
-    // if there are previous results or errors, remove them
-    console.log(responseJson);
-    $('#js-error-message').empty();
+// function displayResults(responseJson) {
+
+    // const selectedStates = $('input[type=checkbox]:checked').map(function() {
+    //      return this.id;
+    //      }).get().join(',');
+    //      getParks(selectedStates, maxResults);
+    //      // Clear the inputs and disable the submit button
+    //      $('input:checkbox').prop('checked', false);
+    //      $('input[type=submit').prop('disabled', true);
+    //     });
+    // }
   
     // iterate over the response
     // for (let i = 0; i < responseJson.data.length ; i++){
@@ -61,17 +68,85 @@ function displayResults(responseJson) {
   
 
     // };
-};
-
-// $(getData);
-$(showMenu);
-
-// FOR LATER
-
-// function shuffle(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     return array;
 // };
+
+function displayResults(responseJson) {
+
+    console.log(responseJson);
+    console.log(responseJson.entries.length);
+    $('#js-error-message').empty();
+
+    // Listen for a click on the .categories fieldset
+    $('.categories').on('click', 'input', (function(event) {
+    console.log('Click recorded.')
+    console.log($(this).prop('checked'))
+    
+    // When it comes, check if the checkbox that has been clicked on 
+    // is being checked or unchecked. If it's checked,
+    if ($(this).prop('checked') === true) {
+        console.log('Checked.');
+        console.log($(this).parent().text());
+
+        // Loop through the responseJson 
+        for (let i = 0; i < responseJson.entries.length; i++){
+            
+            // And if the text of the label of the checkbox that has been
+            // clicked on is equal to the text of the category key in the
+            // JSON object,
+            if ($(this).parent().text() === responseJson.entries[i].Category) {
+                console.log($(this).parent().text());
+
+                // Generate an <li>, give it a class of i, and append it 
+                // to <ul class="results-list">
+                $('.results-list').append(
+                    `<li class="result ${i}">
+                        <h3><a href="${responseJson.entries[i].Link}" target="_blank">${responseJson.entries[i].API}</a></h3>
+                        <p class="description">${responseJson.entries[i].Description}.</p>
+                        <ul class="result-options">
+                            <li class="result-option">
+                                <span class="option-name">CORS</span> 
+                                <span class="option-value">${responseJson.entries[i].Cors}</span>
+                            </li>
+                            <li class="result-option">
+                                <span class="option-name">Auth</span> 
+                                <span class="option-value">${responseJson.entries[i].Auth}</span>
+                            </li>
+                            <li class="result-option">
+                                <span class="option-name">HTTPS</span>
+                                <span class="option-value">${responseJson.entries[i].HTTPS}</span></li>
+                        </ul>
+                    </li>`
+
+                )
+            }
+        }
+         
+    // If it's unchecked,
+    } else if ($(this).prop('checked') === false) {
+        console.log('Unchecked.');
+        console.log($(this).parent().text());
+
+        // Loop through the responseJson
+        for (let i = 0; i < responseJson.entries.length; i++) {
+
+            // And remove anything classed i for which the text of the label of the // checkbox that has been clicked on is equal to the text of the
+            // category key in the JSON object
+            if ($(this).parent().text() === responseJson.entries[i].Category) {
+                $(`.${i}`).remove();
+            }
+        }
+        
+    }
+
+    }));
+}
+
+// Clear the checkboxes on page reload
+function clearInputs() {
+    $('input:checkbox').prop('checked', false);
+}
+
+$(getData);
+$(showMenu);
+// $(listenForClick);
+$(clearInputs);
